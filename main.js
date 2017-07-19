54867ddbe1e81db1b2fdc0e57df7c9072e144a45
Adi <adij82@gmail.com>
2017-07-19 15:14:49 +0300

    Minor fixes

--- main.js
+++ main.js
@@ -1,16 +1,4 @@
-// Google maps feature:
 
-function initMap() {
-  var uluru = {lat: -34.589973, lng: -58.443546};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: uluru
-  });
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });
-}
 
 function scrollFunction(backToTopBtn) {
   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
@@ -19,7 +7,17 @@ function scrollFunction(backToTopBtn) {
     backToTopBtn.style.display = "none";
   }
 }
+function scrollAndBackToTop() {
+  const backToTopBtn = document.querySelector('.backto-top');
+  window.onscroll = ()=> {
+    scrollFunction(backToTopBtn)
+  };
 
+  backToTopBtn.addEventListener('click', () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
+}
 function chatClickHandler(chatPopup,xChat) {
   chatPopup.style.display = 'flex';
   let timeLeft = 10;
@@ -35,23 +33,7 @@ function chatClickHandler(chatPopup,xChat) {
   }, 1000);
 }
 
-
-
-
-function initPage() {
-
-  //   Scroll
-  const backToTopBtn = document.querySelector('.backto-top');
-  window.onscroll = ()=> {
-    scrollFunction(backToTopBtn)
-  };
-
-  backToTopBtn.addEventListener('click', () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  });
-
-  //  Chat
+function chat() {
   const bannerChat = document.querySelector('.banner-chat');
   const chatOnVivo = document.querySelector('.chat-on-vivo');
   const chatPopup = document.querySelector('.chat-pop-up');
@@ -63,8 +45,8 @@ function initPage() {
   xChat.addEventListener('click', () => {
     chatPopup.style.display = 'none';
   });
-
-  // Burger menu
+}
+function burgerMenuClicksHandlers() {
   const showMenuIcon = document.querySelector('.burger-menu-icon');
   const hideMenuIcon = document.querySelector('.close-menu-icon');
   const burgerMenu = document.querySelector('.burger-list');
@@ -82,4 +64,44 @@ function initPage() {
   });
 }
 
+function resizeFonts() {
+  const htmlElem = document.querySelector('html');
+
+  const smallA = document.querySelector('.small-a');
+  smallA.addEventListener('click', ()=> {
+    htmlElem.style.fontSize = '70%'
+  });
+  const medA = document.querySelector('.medium-a');
+  medA.addEventListener('click', ()=> {
+    htmlElem.style.fontSize = '85%'
+  });
+  const bigA = document.querySelector('.big-a');
+  bigA.addEventListener('click', ()=> {
+    htmlElem.style.fontSize = '100%'
+  });
+}
+
+
+function initPage() {
+
+  scrollAndBackToTop();
+  chat();
+  burgerMenuClicksHandlers();
+  resizeFonts();
+}
+
 initPage();
+
+// Google maps feature:
+
+function initMap() {
+  var uluru = {lat: -34.589973, lng: -58.443546};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
